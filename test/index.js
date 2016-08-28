bf7a968450af0d47c9bfc6c785af5b184e7b50f5
Nick Bartlett <ntbartle@us.ibm.com>
2016-08-28 02:53:53 -0400

    :new: test for number of lines changed

--- test/index.js
+++ test/index.js
@@ -4,8 +4,18 @@
 const expect = require('chai').expect;
 const locDown = require('../index');
 
-describe('loc-down', () => {
-  it('does nothing', () => {
-    expect(locDown()).to.equal(true);
+describe('The utility for testing lines of code changed', () => {
+  const maxLOC = 300;
+  const remote = 'git@github.com:tteltrab/loc-down.git';
+  const branch = 'master';
+
+  it('returns true if the number of LOC changed between current HEAD and the remote is less ' +
+    'than the maximum LOC number indicated', done => {
+    locDown(remote, branch, maxLOC)
+      .then(diff => {
+        expect(diff).to.equal(true);
+        done();
+      })
+      .catch(done);
   });
 });
