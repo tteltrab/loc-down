db6bc29cb7d89b74500a33f636451d3aa4ad6eaa
Nick Bartlett <ntbartle@us.ibm.com>
2016-08-28 17:01:17 -0400

    :bug: remove defualt branch

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ const randomString = require('randomstring');
  * diffs the branch on the repo with the current HEAD,
  * failing if the diff has more than `loc` lines of code changed
  * @param  {string} repo   - url representation of the repo
- * @param  {string} branch - name of the branch to do the diff against, defualts to `master`
+ * @param  {string} branch - name of the branch to do the diff against
  * @param  {number} loc    - maximum number of lines of code changed
  * @returns {boolean}      - true if number of changes is lte `loc`
  */
-module.exports = function checkLOC (repo, branch = 'master', loc) {
+module.exports = function checkLOC (repo, branch, loc) {
   return new Promise((resolve, reject) => {
     const remoteName = randomString.generate();
 
